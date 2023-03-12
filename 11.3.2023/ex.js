0de33ef98791901e45eb5bf699a58d6337ea9c9f
ThureinMyoMin <crosshunted62@gmail.com>
2023-03-12 14:48:52 +0630

    add new

---
+++ 11.3.2023/ex.js
@@ -0,0 +1,18 @@
+console.log("hello word");
+console.log("My name is shoon let thu");
+// String ,number,booLean, (primative data type)
+var a = 1;
+var b = 2;
+var c = a + b;
+console.Log(c);
+console.log(typeof prime);
+console.log(typeof a);
+console.log(typeof b);
+var num1 = "4";
+var num2 = "6";
+var total = Number(num1) + Number(num2);
+conole.log(total);
+console.log(typeof num1);
+console.log("YOUR name is " + name + ".YOUR aye " + Age);
+// console.log(`your name is ${name}.you are $ {age}`;
+// var addredd=
---
+++ 12.3.2023/browser.js
@@ -0,0 +1,7 @@
+// let name = prompt("Enter your name:");
+// let age = prompt("Enter your age");
+// alert(`Welcome ${name}!you are $ {age}`);
+let num1 = prompt("Enter yourfirt num ");
+let num2 = prompt("Enter yoursecond num");
+let sum = num1 + num2;
+alert(`THE sum OF ${num1}and ${num2} is ${sum}`);
